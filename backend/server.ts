5ba7f7f9de0fe6c74fef82ed075931f651a6d311
msteknoadam <yesilyurt.gok@gmail.com>
2020-02-22 16:41:03 +0300

    Remove unnecessary console log

--- backend/server.ts
+++ backend/server.ts
@@ -53,9 +53,9 @@ const initializeServer = async () => {
 	});
 
 	io.on("connection", socket => {
-		console.log("A user connected.");
+		logger.info(`User #${socket.id} connected.`);
 		socket.on("disconnect", () => {
-			console.log("A user disconnected.");
+			logger.info(`User #${socket.id} disconnected.`);
 		});
 	});
 };
